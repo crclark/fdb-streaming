cb34e661fd83db037f3c709681207813ab9ec3cd
Connor Clark <connor.r.clark@gmail.com>
2019-07-20 12:25:40 -0700

    Fix preconditions to prevent errors

--- test/Spec/FDBStreaming/TaskLease.hs
+++ test/Spec/FDBStreaming/TaskLease.hs
@@ -16,15 +16,12 @@ module Spec.FDBStreaming.TaskLease where
 
 import Control.Concurrent
 import Control.Monad.IO.Class (liftIO)
-import Debug.Trace
 import FDBStreaming.TaskLease
 import FoundationDB (runTransaction, Database, rangeKeys, clearRange)
 import FoundationDB.Layer.Subspace
 import FoundationDB.Layer.Tuple
 import Data.Functor.Classes (Eq1)
 import Data.Kind (Type)
-import Data.Map (Map)
-import qualified Data.Map as M
 import Safe (headMay)
 import Test.QuickCheck (Property, (===))
 import Test.QuickCheck.Gen
@@ -38,35 +35,16 @@ import qualified Test.StateMachine.Types.Rank2 as Rank2
 update :: Eq a => a -> b -> [(a, b)] -> [(a, b)]
 update ref i m = (ref, i) : filter ((/= ref) . fst) m
 
-testTaskSpace :: TaskSpace
-testTaskSpace = taskSpace (subspace [ Bytes "fdbstreaming-test"
-                                    , Bytes "leases-state-machine"])
-
 testSS :: Subspace
 testSS = subspace [Bytes "fdbstreaming-test"]
+
+testTaskSpace :: TaskSpace
+testTaskSpace = taskSpace (extend testSS [Bytes "leases-state-machine"])
 
 cleanup :: Database -> IO ()
 cleanup db = do
   let (begin, end) = rangeKeys $ subspaceRange testSS
   runTransaction db $ clearRange begin end
-
-newtype OutstandingVersions = OutstandingVersions (Map AcquiredLease Int)
-  deriving (Show, Eq, Ord)
-
-getOutstandingVersions :: OutstandingVersions -> [AcquiredLease]
-getOutstandingVersions (OutstandingVersions m) = M.keys m
-
-addOutstandingVersion :: OutstandingVersions -> AcquiredLease -> OutstandingVersions
-addOutstandingVersion (OutstandingVersions m) lv = OutstandingVersions $
-  M.insertWith (+) lv 1 m
-
-remOutstandingVersion :: OutstandingVersions -> AcquiredLease -> OutstandingVersions
-remOutstandingVersion (OutstandingVersions m) lv = OutstandingVersions $
-  M.update (\n -> if n == 1 then Nothing else Just (n - 1)) lv m
-
-isDuplicatedVersion :: OutstandingVersions -> Bool
-isDuplicatedVersion (OutstandingVersions m) =
-  M.foldr max 0 m > 1
 
 -- NOTE: since the TaskName type is so easy to create (just a string), we don't
 -- actually need the Reference utilities that QSM provides. The r is simply a
@@ -100,14 +78,14 @@ data Response (r :: Type -> Type)
 semantics :: Database -> Command Concrete -> IO (Response Concrete)
 semantics db (TryAcquireFor n ref) =
   runTransaction db $ tryAcquire testTaskSpace ref n >>= \case
-    Nothing          -> return AlreadyLocked
-    Just lease       -> return (Acquired lease)
+    Nothing    -> return AlreadyLocked
+    Just lease -> return (Acquired lease)
 
-semantics db (Deliver lv ref) =
-  runTransaction db $ release testTaskSpace ref lv >>= \case
-    ReleaseSuccess  -> return Success
-    AlreadyExpired  -> return Expired
-    InvalidLease    -> return LeaseDNE
+semantics db (Deliver lease ref) =
+  runTransaction db $ release testTaskSpace ref lease >>= \case
+    ReleaseSuccess -> return Success
+    AlreadyExpired -> return Expired
+    InvalidLease   -> return LeaseDNE
 
 semantics _ (PassTime n) = do
   threadDelay (n * 1000_000)
@@ -123,26 +101,26 @@ semantics db (AcquireRandom n) = do
     Nothing -> return AlreadyLocked
     Just (taskName, lease) -> return (AcquiredRandom taskName lease)
 
-data IsLocked =
+data LeaseState =
   IsLocked AcquiredLease
   | LastLeaseWas AcquiredLease
   | NeverLocked
   deriving (Show, Eq, Generic)
 
-deriving instance ToExpr IsLocked
+deriving instance ToExpr LeaseState
 
-isLocked :: IsLocked -> Bool
+isLocked :: LeaseState -> Bool
 isLocked (IsLocked _) = True
 isLocked _ = False
 
-isAvailable :: IsLocked -> Bool
+isAvailable :: LeaseState -> Bool
 isAvailable = not . isLocked
 
 -- | Models a set of locks. Each is either locked (True) or not, and has a
 --   lockversion. NOTE: because TaskNames are just strings, we don't even need
 --   the Reference machinery. r is just a phantom type to get things in the
 --   shape QSM expects.
-data Model r = Model [(TaskName, IsLocked)]
+newtype Model r = Model [(TaskName, LeaseState)]
   deriving (Generic, Show)
 
 deriving instance ToExpr AcquiredLease
@@ -152,8 +130,12 @@ deriving instance ToExpr (Model Concrete)
 initModel :: Model r
 initModel = Model []
 
---TODO: think of some preconditions. None immediately leap to mind.
+--NOTE: preconditions are used by qsm to constrain what commands are generated,
+--not to do test assertions! Unfortunately, this is only documented in the
+--github readme, not the haddock docs.
 precondition :: Model Symbolic -> Command Symbolic -> Logic
+precondition (Model []) (EnsureTask _) = Top
+precondition (Model []) _ = Bot .// "When no tasks exist, can only run EnsureTask"
 precondition (Model refs) (TryAcquireFor _ ref) =
   case lookup ref refs of
     Nothing -> Bot .// ("Can't acquire non-existent taskName: " ++ show ref)
@@ -174,14 +156,14 @@ transition m (TryAcquireFor _ _) _ = m
 transition (Model refs) (Deliver acquiredLease ref) Success =
   Model (update ref (LastLeaseWas acquiredLease) refs)
 transition m (Deliver _ _) _ = m
-transition m (PassTime _) _ = m
+transition m (PassTime _) _  = m
 transition m@(Model refs) (EnsureTask tn) (TaskEnsured result) =
   case (lookup tn refs, result) of
     (Nothing, AlreadyExists _) -> error "existing task untracked by model!"
-    (Nothing, NewlyCreated _) -> Model ((tn, NeverLocked):refs)
-    (Just _, NewlyCreated _) -> error "task exists in model but not in DB!"
-    (Just _, AlreadyExists _) -> m
-transition m (EnsureTask _) _ = m
+    (Nothing, NewlyCreated _)  -> Model ((tn, NeverLocked):refs)
+    (Just _, NewlyCreated _)   -> error "task already existed in model but not in DB!"
+    (Just _, AlreadyExists _)  -> m
+transition _ (EnsureTask _) x = error $ "impossible EnsureTask transition: " ++ show x
 transition
   (Model refs) (AcquireRandom _) (AcquiredRandom taskName acquiredLease) =
     Model (update taskName (IsLocked acquiredLease) refs)
@@ -210,6 +192,10 @@ postcondition (Model refs) cmd resp = case (cmd, resp) of
       Nothing                -> Top
   (AcquireRandom _, AcquiredRandom taskName _acquiredLease) ->
     case lookup taskName refs of
+      -- TODO: this is wrong. The model doesn't get updated by the passage of
+      -- time, so this can be legal if enough time has passed. As it is, the
+      -- QSM tests aren't testing lock expiry unless we get really lucky with
+      -- the sequence of commands that get generated.
       Just oldLockStatus -> isAvailable oldLockStatus .== True .// "can only acquire available tasks"
       Nothing -> error $ "acquired lock that DNE in Model: " ++ show taskName
   (AcquireRandom _, AlreadyLocked) ->
@@ -221,6 +207,9 @@ postcondition (Model refs) cmd resp = case (cmd, resp) of
 generator :: Model Symbolic -> Maybe (Gen (Command Symbolic))
 generator (Model refs) = Just $
   if null refs
+    -- TODO: for reasons that remain mysterious, qsm is somehow generating
+    -- command sequences that don't start with EnsureTask. To see this happen,
+    -- remove the preconditions above that prevent it.
     then frequency
            [ (1, pure $ EnsureTask "task1")
            , (1, pure $ EnsureTask "task2")
@@ -229,7 +218,7 @@ generator (Model refs) = Just $
       (ref, mLease) <- elements refs
       frequency $
         [ (10, pure $ TryAcquireFor 5 ref)
-        , (10, pure $ PassTime 1)
+        , (2, pure $ PassTime 1)
         , (10, pure $ AcquireRandom 5)
         ]
         ++
